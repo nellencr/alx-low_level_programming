f43bdf8a0eb394301f566af6e5843403d44e0285
nellencr <nellencr@gmail.com>
2022-09-14 16:19:08 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,15 +7,22 @@
  */
 int print_sign(int n)
 {
+	int num;
 
-	if (n >= 0)
+	if (n > 0)
 	{
-		return (1);
+		num = 1;
+		_putchar('+');
 	}
-	else if (n <= 0)
+	else if (n < 0)
 	{
-		return (-1);
+		num = -1;
+		_putchar('-');
 	}
 	else
-		return (0);
+	{
+		num = 0;
+		_putchar('0');
+	}
+	return (num);
 }
