d7a0bebf7283f39d27be2613e4363bac1a69cdca
nellencr <nellencr@gmail.com>
2022-09-15 07:01:03 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_last_digit - print the last digit of iput number.
+ * @n: input number.
+ * Return: the last value.
+ */
+int print_last_digit(int n)
+{
+	int last_digit;
+
+	if (n < 0)
+	{
+		last_digit = -1 * (n % 10);
+	}
+	else
+		last_digit = n % 10;
+
+	_putchar(last_digit + '0');
+	return (last_digit);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -4,3 +4,4 @@ Function that checks for lowercase character
 unction that checks for alphabetic character.
 Function that prints the sign of a number.
 Function that computes the absolute value of an integer.
+Function that prints the last digit of a number.
