2dce6cb1323815030c171a5a52b4f3d7c1090240
nellencr <nellencr@gmail.com>
2022-09-17 02:33:38 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - a function that prints the numbers, from 0 to 9
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int i = '0';
+
+	for (; i <= '9'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
+
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,3 +1,4 @@
 Function that checks for uppercase character.
 Function that checks for a digit (0 through 9).
 Function that multiplies two integers.
+unction that prints the numbers, from 0 to 9, followed by a new line.
