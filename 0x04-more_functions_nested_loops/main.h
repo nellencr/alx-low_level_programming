46fa9dc8589844194faa84092be6c7261dcfc518
nellencr <nellencr@gmail.com>
2022-09-17 12:56:44 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * main - header file for protoypes
+ * void protoypes (void)
+ * @c: An input integer
+ * @a: An input integer
+ * @b: An input integer
+ * int protoypes (int)
+ */
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
+void print_line(int n);
+#endif /* MAIN_H */
