c0f4f4ee8a2b752836f8799447f116606a40f98d
nellencr <nellencr@gmail.com>
2022-09-19 10:56:44 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - a function that reverses a string.
+ * @s: char input
+ * Return: string
+ */
+
+void rev_string(char *s)
+{
+	int len = 0, i = 0;
+	char temp;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len--)
+	{
+		temp = s[i];
+		s[i++] = s[len];
+		s[len] = temp;
+	}
+
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -3,3 +3,4 @@ Function that swaps the values of two integers.
 2-strlen.c - a function that returns the length of a string.
 3-puts.c - a function that prints a string, followed by a new line, to stdout
 4-print_rev.c -  a function that prints a string, in reverse,
+5-rev_string.c-  a function that reverses a string
