83fa4f1bb056e6bc8c8c9d5e6e12a41c8c437763
nellencr <nellencr@gmail.com>
2022-09-24 06:40:39 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * string_toupper - a function that changes all lowercase letters
+ * a string to uppercase.
+ * @n: an input string
+ * Return: upper case string
+ */char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+		i++;
+	}
+	return (s);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -4,3 +4,4 @@
 2-strncpy.c - a function that copies a string.
 3-strcmp.c - a function that compares two strings.
 4-rev_array.c - a function that reverses the content of an array of integers.
+ 5-string_toupper.c - a function that changes all lowercase letters of a string to uppercase.
