864a3fadc9a3864758b73383ad3627c86598b18c
nellencr <nellencr@gmail.com>
2022-09-24 07:09:47 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+/**
+ *  void prototypes()
+ *  int prototypes()
+ *  char prototypes()
+ */
 char *_strcat(char *dest, char *src);
 int _putchar(char c);
 char *_strncat(char *dest, char *src, int n);
@@ -5,3 +13,6 @@ char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *s);
+char *cap_string(char *);
+
+#endif /*MAIN_H*/
