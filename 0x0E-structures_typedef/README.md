5955cdb13276a86512b756e9489d001575eec08d
nellencr <nellencr@gmail.com>
2022-10-10 04:34:50 -0700

    dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,4 @@
+0. Poppy - Define a new type struct dog with the following elements:
+1-initdog.c - a function that initialize a variable of type struct dog
+2-printdog.c - a function that prints a struct dog
+
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct for dog info
+ * @name: type char
+ * @age: float type
+ * @owner: char type
+ *
+ *Description: structure to store dog info
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+#endif
