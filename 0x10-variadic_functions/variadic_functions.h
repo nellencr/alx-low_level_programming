90f7fa1ddd58233947e49ebd426c1c1ca61425ee
nellencr <nellencr@gmail.com>
2022-10-13 11:14:38 -0700

    3-print_all.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,14 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include <stdarg.h>
+#include <stdio.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-
+void print_all(const char * const format, ...);
+char *make_nil(char *s);
+int count_format(const char * const format);
+void print_comma(int j, int x);
 #endif
