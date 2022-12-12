ab00196a82f89603cc1cdc43482ab0054efcafa9
nellencr <nellencr@gmail.com>
2022-12-12 20:48:11 +0200

    files

---
+++ 0x18-dynamic_libraries/my_functions.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+/**
+ * add - add two integers
+ * @a:input integer
+ * @b: input integer
+ * Return: what the function does
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - substract two integers
+ * @a:input integer
+ * @b: input integer
+ * Return: what the function does
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiply two integers
+ * @a:input integer
+ * @b: input integer
+ * Return: what the function does
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divide two integers
+ * @a:input integer
+ * @b: input integer
+ * Return: what the function does
+ *
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - give remainder of two integers
+ * @a:input integer
+ * @b: input integer
+ * Return: what the function does
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
